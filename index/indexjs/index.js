f3e13f7d915c3f69bf28ca1b420c748e55f6319a
hxj <1760809046@qq.com>
2022-11-01 23:48:02 +0800

    '一半'

---
+++ index/indexjs/index.js
@@ -0,0 +1,80 @@
+// nav部分
+var reply_=document.getElementsByClassName('nav_a')[0];
+var qul_=document.getElementsByClassName('qul')[0].firstChild.nodeValue;
+var pro_=document.getElementsByClassName('pro')[0].firstChild.nodeValue;
+reply_.onclick=function(){
+    var pro=document.getElementsByClassName('pro')[0];
+    var qul=document.getElementsByClassName('qul')[0];
+    if(pro_>0){
+        pro_--;
+        pro.innerHTML=pro_;
+        qul_++;
+        qul.innerHTML=qul_;
+    }else{
+        pro.innerHTML=0;
+    }
+    
+}
+// 热门试用 轮播图
+var slide_list=document.getElementsByClassName('slide_list');
+var hot_slide=document.getElementsByClassName('hot_slide')[0];
+var timer=null;//ul切换
+var timer1=null;//ul分步，动起来
+var index=0;//ul下标
+function move(){
+    timer=setInterval(function(){
+        index++;
+        if(index>=slide_list.length){
+            index=0;
+        }
+        var step=0;
+        var maxstep=40;
+        var start=hot_slide.scrollLeft;
+        var end=slide_list[0].offsetWidth*index;
+        var everystep=(end-start)/maxstep;
+        timer1=setInterval(function(){
+            step++;
+            if(step>=maxstep){
+                step=0;
+                clearInterval(timer1);
+            }
+            start +=everystep;
+            hot_slide.scrollLeft=start;
+        },12)
+        var pre=document.getElementsByClassName('pre')[0];
+        var next=document.getElementsByClassName('next')[0];
+        pre.onclick=function(){
+            clearInterval(timer);
+            index--;
+            timer1=setInterval(function(){
+                step--;
+                if(step>=maxstep){
+                    step=0;
+                    clearInterval(timer1);
+                }
+                start -=everystep;
+                hot_slide.scrollLeft=start;
+            },12)
+            
+        }
+        next.onclick=function(){
+            clearInterval(timer); 
+            index++;
+            timer1=setInterval(function(){
+                step++;
+                if(step>=maxstep){
+                    step=0;
+                    clearInterval(timer1);
+                }
+                start +=everystep;
+                hot_slide.scrollLeft=start;
+            },12)
+        }
+        hot_slide.onmouseenter=function(){
+            clearInterval(timer);
+            clearInterval(timer1);
+            move();
+        }
+    },2500)
+}
+move();
