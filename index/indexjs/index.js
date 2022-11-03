d993f91734765a3773921980f0380d8eddf41951
hxj <1760809046@qq.com>
2022-11-03 17:34:58 +0800

    ''

--- index/indexjs/index.js
+++ index/indexjs/index.js
@@ -1,4 +1,3 @@
-
 // nav部分
 var reply_=document.getElementsByClassName('nav_a')[0];
 var qul_=document.getElementsByClassName('qul')[0].firstChild.nodeValue;
@@ -13,9 +12,35 @@ reply_.onclick=function(){
         qul.innerHTML=qul_;
     }else{
         pro.innerHTML=0;
+        reply_.className='nav_a_a';
+        reply_.innerHTML='试用产品剩余0'
     }
     
 }
+// 时间
+var nav_p=document.getElementsByClassName('nav_p')[0];
+function timego(){
+    var nowTime=new Date();
+    var goTime=new Date(2022,11,1);
+    var dif=goTime-nowTime;
+    function fn(a){
+        return a>10?a:'0'+a;
+    }
+    var day=parseInt(dif/1000/60/60/24);
+    var day1=fn(day);
+    var h=parseInt(dif/1000/60/60%24);
+    var h1=fn(h);
+    var m=parseInt(dif/1000/60%60);
+    var m1=fn(m);
+    str='申请时间剩余：'+day1+'天'+h+'小时'+m+'分钟';
+    nav_p.innerHTML=str;
+    if(day<=0&&h<=0&&m<=0){
+        clearInterval(timer);
+        nav_p.innerHTML='试用申请结束';
+    }
+}
+timego()
+var timer=setInterval(timego,1000);
 // 热门试用 轮播图
 var slide_list=document.getElementsByClassName('slide_list');
 var hot_slide=document.getElementsByClassName('hot_slide')[0];
@@ -44,38 +69,38 @@ function move(){
         },12)
         var pre=document.getElementsByClassName('pre')[0];
         var next=document.getElementsByClassName('next')[0];
-        pre.onclick=function(){
-            clearInterval(timer);
-            index--;
-            timer1=setInterval(function(){
-                step--;
-                if(step>=maxstep){
-                    step=0;
-                    clearInterval(timer1);
-                }
-                start -=everystep;
-                hot_slide.scrollLeft=start;
-            },12)
+        // pre.onclick=function(){
+        //     clearInterval(timer);
+        //     index--;
+        //     timer1=setInterval(function(){
+        //         step--;
+        //         if(step>=maxstep){
+        //             step=0;
+        //             clearInterval(timer1);
+        //         }
+        //         start -=everystep;
+        //         hot_slide.scrollLeft=start;
+        //     },12)
             
-        }
-        next.onclick=function(){
-            clearInterval(timer); 
-            index++;
-            timer1=setInterval(function(){
-                step++;
-                if(step>=maxstep){
-                    step=0;
-                    clearInterval(timer1);
-                }
-                start +=everystep;
-                hot_slide.scrollLeft=start;
-            },12)
-        }
-        hot_slide.onmouseenter=function(){
-            clearInterval(timer);
-            clearInterval(timer1);
-            move();
-        }
+        // }
+        // next.onclick=function(){
+        //     clearInterval(timer); 
+        //     index++;
+        //     timer1=setInterval(function(){
+        //         step++;
+        //         if(step>=maxstep){
+        //             step=0;
+        //             clearInterval(timer1);
+        //         }
+        //         start +=everystep;
+        //         hot_slide.scrollLeft=start;
+        //     },12)
+        // }
+        // hot_slide.onmouseenter=function(){
+        //     clearInterval(timer);
+        //     clearInterval(timer1);
+        //     move();
+        // }
     },2500)
 }
 move();
@@ -89,9 +114,25 @@ for (var i=0;i<like1.length;i++){
         like1a ++;
         this.innerHTML=like1a;
     }
-    words1[i].onclick=function(){
-        words1a ++;
-        this.innerHTML=words1a;
+    // words1[i].onclick=function(){
+    //     words1a ++;
+    //     this.innerHTML=words1a;
+    // }
+}
+var like2=document.getElementsByClassName('shoppers_item1_sp1');
+var words2=document.getElementsByClassName('shoppers_item1_sp2');
+for (var i=0;i<like2.length;i++){
+    var like2a=document.getElementsByClassName('shoppers_item1_sp1')[i].firstChild.nodeValue-0;
+    var words2a=document.getElementsByClassName('shoppers_item1_sp2')[i].firstChild.nodeValue-0;
+    like2[i].onclick=function(){
+        like2a ++;
+        this.innerHTML=like2a;
+    }
+    like2[i].onmouseenter=function(){
+        this.className='shoppers_item1_sp11';
+    }
+    like2[i].onmouseleave=function(){
+        this.className='shoppers_item1_sp1';
     }
 }
 // 发现酷玩
@@ -103,14 +144,13 @@ var dataList=[];
     //2、ajax请求 请求后将数据放入数组dataList
 function getData(){
     var ajax=new XMLHttpRequest()||new ActiveXObject('Microsoft.XMLHTTP');
-    ajax.open('get','../../jiguo_server/public/data/play_new.json');
+    ajax.open('get','http://127.0.0.1:3000/play/new');
     ajax.send();
     ajax.onreadystatechange=function(){
         if(ajax.readyState==4){
             if(ajax.status==200){
                 data=ajax.responseText;
                 dataList=JSON.parse(data);
-                console.log(dataList);
                 show();
             }else{
                 console.log('加载错误');
@@ -123,28 +163,32 @@ var index=-1;
 var main_box=document.getElementsByClassName('main_box')[0];
     // 开关
 var flg=true;
+    //加载a
+var load=document.getElementsByClassName('load')[0];
     // 3、调用展示数据的方法
 function show(){
     if(flg){
         index++;
-        console.log(index);
-        console.log(dataList.length);
-        var load=document.getElementsByClassName('load')[0];
-        if(index>=dataList.length){
-            load.innerHTML='数据加载完毕';
-            return flg=false;
-        }
-        var playmore=document.getElementsByClassName('playmore')[0];
-        if(!ul){
-            playmore.style.display='block';
-        }
+        // if(index>=dataList.length){
+        //     clearTimeout(getData,300);
+        //     return flg=false;
+        //     // load.innerHTML='数据加载完毕'; 
+        // }
         var ul=document.createElement('ul');
         ul.setAttribute('class','main_list');
+        var ull=document.getElementsByClassName('main_list');
+        if(ull.length>=dataList.length){
+            clearTimeout(getData,300);
+            load.innerHTML='~~数据加载完毕'; 
+            load.style.background='none';
+            load.style.color='grey';
+            return flg=false;  
+        }
         for(var item of dataList[index]){
-            console.log(item);
             // li a
             var li=document.createElement('li');
             var a=document.createElement('a');
+            a.setAttribute('class','main_list_a');
             li.appendChild(a);
             ul.appendChild(li);
             // img div1(main_list_box)
@@ -184,23 +228,19 @@ function show(){
             div3.appendChild(span3);
             div3.appendChild(span4);
         }
-        setTimeout(function(){
-            if(ul){
-                playmore.style.display='none';
-            }
-        },500)
         main_box.appendChild(ul);
+        
     }
 }
-    // 4、触底加载
-window.onscroll=function(){
-    var windowHeight=document.documentElement.clientHeight||document.body.clientHeight;
-    var scrollTop=document.documentElement.scrollTop||document.body.scrollTop;
-    var pageHeight=document.documentElement.scrollHeight||document.body.scrollHeight;
-    if(windowHeight+scrollTop>=pageHeight){
-        setTimeout(show,500)
-    }
+// 4、点击加载
+load.onclick=function(){
+    load.className='load_con';
+    setTimeout(show,700)  
+    load.onmouseleave=setTimeout(function(){
+        load.className='load';
+    },700)
 }
+
 
 
 
