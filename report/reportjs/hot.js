294771c47429ac47a37f2fa2f87ea24bdf4a30c7
王鑫雨 <487708747@qq.com>
2022-11-05 19:53:02 +0800

    '添加了usejs'

---
+++ report/reportjs/hot.js
@@ -0,0 +1,84 @@
+//获取整个div
+var div = document.getElementsByClassName('center')[0];
+
+window.onload = function () {
+    setTimeout(ajax_, 300)
+}
+//创建数组接受后台数据
+var datalist = [];
+
+//创建ajax请求数据
+function ajax_() {
+    var ajax = new XMLHttpRequest() || new ActiveXObject('Microsoft.XMLHTTP');
+    ajax.open('get', 'http://127.0.0.1:3000/report/new');
+    ajax.send();
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4) {
+            if (ajax.status == 200) {
+                data = ajax.responseText;
+                datalist = JSON.parse(data);
+                // datalist = JSON.parse(ajax);
+                show();
+            } else {
+                console.log('加载失败');
+            }
+        }
+    }
+}
+
+var index = 0;
+function show() {
+    for (var item of datalist) {
+        //创建ul
+        var ul = document.createElement('ul');
+        ul.className = 'distance';
+        //创建ul下的li
+        var ul_li = document.createElement('li');
+        ul.appendChild(ul_li);
+        ul_li.className = 'centerleft';
+        //创建ul li 下的 img 
+        var ul_li_img = document.createElement('img');
+        ul_li_img.src = item.img;
+        ul_li.appendChild(ul_li_img);
+        //创建ul li 下的 div
+        var ul_li_div = document.createElement('div');
+        ul_li_div.className = 'air';
+        ul_li.appendChild(ul_li_div);
+        //创建 ul li div 下的第一个div
+        var ul_li_div_div1 = document.createElement('div');
+        ul_li_div_div1.className = 'title';
+        ul_li_div.appendChild(ul_li_div_div1);
+        //创建 ul li div 下的第2个div
+        var ul_li_div_div2 = document.createElement('div');
+        ul_li_div_div2.className = 'purifier';
+        ul_li_div.appendChild(ul_li_div_div2);
+        //创建 ul li div 第2个div里的span1
+        var ul_li_div_div2_span1 = document.createElement('span');
+        ul_li_div_div2_span1.className = 'name';
+        ul_li_div_div2.appendChild(ul_li_div_div2_span1);
+        //创建 ul li div 第2个div里的span2
+        var ul_li_div_div2_span2 = document.createElement('span');
+        ul_li_div_div2_span2.className = 'time';
+        ul_li_div_div2.appendChild(ul_li_div_div2_span2);
+        //创建 ul li div 下的第三个div
+        var ul_li_div_div3 = document.createElement('div');
+        ul_li_div_div3.className = 'rightpurifier';
+        ul_li_div.appendChild(ul_li_div_div3);
+        //创建 ul li div 第三个div里的span1
+        var ul_li_div_div3_span1 = document.createElement('span');
+        ul_li_div_div3_span1.className = 'three';
+        ul_li_div_div3.appendChild(ul_li_div_div3_span1);
+        //创建 ul li div 第三个div里的span2
+        var ul_li_div_div3_span2 = document.createElement('span');
+        ul_li_div_div3_span2.className = 'three1';
+        ul_li_div_div3.appendChild(ul_li_div_div3_span2);
+
+        div.appendChild(ul)
+    }
+
+}
+
+var to = document.getElementById('to');
+to.onclick = function () {
+    show()
+}
---
+++ use/usejs/tys.js
@@ -0,0 +1,103 @@
+
+// // 获取整个ul
+var ul = document.getElementsByClassName('box2')[0];
+window.onload = function () {
+    setTimeout(ajax_, 300)
+}
+// // 创建数组接受后台数据
+var datalist = [];
+// 创建ajax请求数据
+function ajax_() {
+    var ajax = new XMLHttpRequest() || new ActiveXObject("Microsoft.XMLHTTP");
+    ajax.open("get", "http://127.0.0.1:3000/useing/public", true);
+    ajax.send();
+    ajax.onreadystatechange = function () {
+        if (ajax.readyState == 4) {
+            if (ajax.status == 200) {
+
+                datalist = JSON.parse(ajax.response);
+                show();
+
+            }
+        }
+    }
+}
+
+var index = 0;
+// 遍历数组
+function show() {
+    for (var item of datalist) {
+        console.log(item);
+        // 创建li
+        var li = document.createElement("li");
+        li.className = 'box3';
+        // 创建li里面的a标签
+        var a = document.createElement("a");
+        li.appendChild(a);
+        a.href = "./detail.html";
+        // 创建li里面的span标签
+        var span = document.createElement("span");
+        span.innerHTML = item.info_ty;
+        // 设置首发或者体验师的样式类名
+        if (item.info_ty == "首发") {
+            span.className = "publish";
+        } else {
+            span.className = "publish";
+        }
+        a.appendChild(span);
+        // 创建a标签里面的img标签
+        var a_img = document.createElement("img");
+        a_img.src = item.img;
+        a_img.className = "content_pic";
+        a.appendChild(a_img);
+        // 创建a标签里面的div标签
+        var a_div = document.createElement("div");
+        a_div.className = "bread";
+        a.appendChild(a_div);
+
+        // 创建div标签里面的h2标签
+        var a_div_h2 = document.createElement("h2");
+
+        a_div.appendChild(a_div_h2);
+        a_div_h2.className = 'hw';
+        a_div_h2.innerHTML = item.text;
+        // 创建div标签里面的第一个p标签
+        var a_div_p1 = document.createElement("p");
+        a_div_p1.className = 'stage';
+        a_div.appendChild(a_div_p1);
+        // 创建div标签里面的第一个p标签下span1
+        var a_div_p1_span1 = document.createElement("span");
+        a_div_p1.appendChild(a_div_p1_span1);
+        a_div_p1_span1.innerHTML = item.totalnum;
+        // 创建div标签里面的第1个p标签下span2
+        var a_div_p1_span2 = document.createElement("span");
+        a_div_p1.appendChild(a_div_p1_span2);
+        a_div_p1_span2.innerHTML = item.totalnum + '台';
+
+        // 创建div标签里面的第2个p标签
+        var a_div_p2 = document.createElement("p");
+        a_div.appendChild(a_div_p2);
+        a_div_p2.className = 'stage1';
+
+
+        var a_div_p2_span = document.createElement("span");
+        a_div_p2.appendChild(a_div_p2_span);
+        a_div_p2_span.innerHTML = item.apply + '申请';
+        // 创建div标签里面的第3个p标签
+        var a_div_p3 = document.createElement("p");
+        a_div.appendChild(a_div_p3);
+        a_div_p3.className = 'stage2';
+        a_div_p3.innerHTML = "剩余时间2天";
+
+        ul.appendChild(li);
+
+    }
+}
+var to = document.getElementById('to');
+
+to.onclick = function () {
+    setTimeout(show, 700);
+
+
+}
+
